5db51164396145553ec820103dafcc518d2cf3da
Syntax <instructor@syntaxtechs.com>
2026-03-24 19:35:27 -0400

    Clean up redundant output statements in class01
    Removed multiple redundant print statements.

--- src/test/java/class01.java
+++ src/test/java/class01.java
@@ -1,8 +1,7 @@
 public class class01 {
     public static void main(String[] args) {
-        System.out.println("i am first project");
-        System.out.println("i am first project");  System.out.println("i am first project");  System.out.println("i am first project");  System.out.println("i am first project");  System.out.println("i am first project");  System.out.println("i am first project");  System.out.println("i am first project");  System.out.println("i am first project");
-        System.out.println("i am first project");  System.out.println("i am first project");  System.out.println("i am first project");
+
+   
 
        
 
